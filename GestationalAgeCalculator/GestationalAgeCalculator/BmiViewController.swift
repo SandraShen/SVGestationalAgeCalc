b2c8c8813609cdf63c2c4bf0ebe323419484014c
Shen yingyin <shen.yingyin@i3desing.co.jp>
2018-08-23 22:11:59 +0900

    BMI画面改修

--- GestationalAgeCalculator/GestationalAgeCalculator/BmiViewController.swift
+++ GestationalAgeCalculator/GestationalAgeCalculator/BmiViewController.swift
@@ -23,40 +23,76 @@ class BmiViewController: BaseViewController {
     @IBOutlet weak var clearBtn: UIButton!
     
     // MARK: member variables
-    var heightBefor = ""
+    var heightBefore = ""
     var heightAfter = "0"
     var weightBefore = ""
     var weightAfter = "0"
     
+    var picker = UIPickerView()
+    var decimalPicker = UIPickerView()
+    
     var height: Double!
     var weight: Double!
     
+    let decimalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    var heightArray: [Int] = {
+        var array: [Int] = []
+        for i in 120...250 {
+            array.append(i)
+        }
+        return array
+    }()
+    var weightArray: [Int] = {
+        var array: [Int] = []
+        for i in 30...250 {
+            array.append(i)
+        }
+        return array
+    }()
+ 
     // MARK: life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.heightBeforDecimal.inputAccessoryView = self.makeDoneButtonToPicker()
-        self.heightAfterDecimal.inputAccessoryView = self.makeDoneButtonToPicker()
-        self.weightBeforDecimal.inputAccessoryView = self.makeDoneButtonToPicker()
-        self.weightAfterDecimal.inputAccessoryView = self.makeDoneButtonToPicker()
+        heightBeforDecimal.inputAccessoryView = makeDoneButtonToPicker()
+        heightAfterDecimal.inputAccessoryView = makeDoneButtonToPicker()
+        weightBeforDecimal.inputAccessoryView = makeDoneButtonToPicker()
+        weightAfterDecimal.inputAccessoryView = makeDoneButtonToPicker()
+        
+        picker.delegate = self
+        picker.dataSource = self
+        decimalPicker.delegate = self
+        decimalPicker.dataSource = self
+        
+        heightBeforDecimal.text = heightArray[30].description
+        heightAfterDecimal.text = decimalArray[0].description
+        heightBefore = heightBeforDecimal.text!
+        heightAfter = heightAfterDecimal.text!
+        
+        weightBeforDecimal.text = weightArray[20].description
+        weightAfterDecimal.text = decimalArray[0].description
+        weightBefore = weightBeforDecimal.text!
+        weightAfter = weightAfterDecimal.text!
+        
+        calc()
     }
     
     // MARK: IBAction
     // 画面表示初期化
     @IBAction func clearBtnTapped(_ sender: UIButton) {
-        self.heightBeforDecimal.text = ""
-        self.heightAfterDecimal.text = ""
-        self.weightBeforDecimal.text = ""
-        self.weightAfterDecimal.text = ""
-        self.bmiResultLbl.text = ""
+        heightBeforDecimal.text = ""
+        heightAfterDecimal.text = ""
+        weightBeforDecimal.text = ""
+        weightAfterDecimal.text = ""
+        bmiResultLbl.text = ""
         
-        self.heightBefor = ""
-        self.heightAfter = "0"
-        self.weightBefore = ""
-        self.weightAfter = "0"
+        heightBefore = ""
+        heightAfter = "0"
+        weightBefore = ""
+        weightAfter = "0"
     }
     
     @IBAction func calcBtnTapped(_ sender: UIButton) {
-        self.calc()
+        calc()
     }
     
     // MARK: methods
@@ -64,44 +100,44 @@ class BmiViewController: BaseViewController {
     private func calc() {
         // BMI = 体重kg / (身長m * 身長m)
         if transferHeight() && transferWeight() {
-            let res = self.weight / pow(self.height * 0.01, 2)
+            let res = weight / pow(height * 0.01, 2)
             let roundRes = round(res * 100) / 100
-            self.bmiResultLbl.text = String(format: "%.2f", roundRes)
+            bmiResultLbl.text = String(format: "%.2f", roundRes)
         }
     }
     
-    func doneButtonAction() {
-        if self.heightBeforDecimal.isFirstResponder {
-            self.heightBefor = self.heightBeforDecimal.text ?? ""
-            self.heightBeforDecimal.resignFirstResponder()
+    @objc func doneButtonAction() {
+        if heightBeforDecimal.isFirstResponder {
+            heightBefore = heightBeforDecimal.text ?? ""
+            heightBeforDecimal.resignFirstResponder()
         }
-        if self.heightAfterDecimal.isFirstResponder {
-            self.heightAfter = self.heightAfterDecimal.text ?? "0"
-            self.heightAfterDecimal.resignFirstResponder()
+        if heightAfterDecimal.isFirstResponder {
+            heightAfter = heightAfterDecimal.text ?? "0"
+            heightAfterDecimal.resignFirstResponder()
         }
-        if self.weightBeforDecimal.isFirstResponder {
-            self.weightBefore = self.weightBeforDecimal.text ?? ""
-            self.weightBeforDecimal.resignFirstResponder()
+        if weightBeforDecimal.isFirstResponder {
+            weightBefore = weightBeforDecimal.text ?? ""
+            weightBeforDecimal.resignFirstResponder()
         }
-        if self.weightAfterDecimal.isFirstResponder {
-            self.weightAfter = self.weightAfterDecimal.text ?? "0"
-            self.weightAfterDecimal.resignFirstResponder()
+        if weightAfterDecimal.isFirstResponder {
+            weightAfter = weightAfterDecimal.text ?? "0"
+            weightAfterDecimal.resignFirstResponder()
         }
     }
     
     private func transferHeight() -> Bool {
-        if self.heightBefor != "" && self.heightAfter != "" {
-            let heightStr = self.heightBefor + "." + self.heightAfter
-            self.height = NumberFormatter().number(from: heightStr) as! Double
+        if heightBefore != "" && heightAfter != "" {
+            let heightStr = heightBefore + "." + heightAfter
+            height = NumberFormatter().number(from: heightStr) as! Double
             return true
         }
         return false
     }
     
     private func transferWeight() -> Bool {
-        if self.weightBefore != "" && self.weightAfter != "" {
-            let weightStr = self.weightBefore + "." + self.weightAfter
-            self.weight = NumberFormatter().number(from: weightStr) as! Double
+        if weightBefore != "" && weightAfter != "" {
+            let weightStr = weightBefore + "." + weightAfter
+            weight = NumberFormatter().number(from: weightStr) as! Double
             return true
         }
         return false
@@ -109,19 +145,108 @@ class BmiViewController: BaseViewController {
 }
 
 extension BmiViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField.tag {
+        case 0:
+            textField.inputView = picker
+            if let text = textField.text, !text.isEmpty {
+                for element in heightArray.enumerated() {
+                    if element.element == Int(text)! {
+                        picker.selectRow(element.offset, inComponent: 0, animated: false)
+                    }
+                }
+            } else {
+                picker.selectRow(30, inComponent: 0, animated: false)
+                textField.text = heightArray[30].description
+            }
+        case 2:
+            textField.inputView = picker
+            if let text = textField.text, !text.isEmpty {
+                for element in weightArray.enumerated() {
+                    if element.element == Int(text)! {
+                        picker.selectRow(element.offset, inComponent: 0, animated: false)
+                    }
+                }
+            } else {
+                picker.selectRow(20, inComponent: 0, animated: false)
+                textField.text = weightArray[20].description
+            }
+        case 1, 3:
+            textField.inputView = decimalPicker
+            if let text = textField.text, !text.isEmpty {
+                for element in decimalArray.enumerated() {
+                    if element.element == Int(text)! {
+                        decimalPicker.selectRow(element.offset, inComponent: 0, animated: false)
+                    }
+                }
+            } else {
+                decimalPicker.selectRow(0, inComponent: 0, animated: false)
+                textField.text = decimalArray[0].description
+            }
+        default:
+            break
+        }
+    }
+    
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         switch textField.tag {
         case 0:
-            self.heightBefor = self.heightBeforDecimal.text ?? ""
+            heightBefore = heightBeforDecimal.text ?? ""
         case 1:
-            self.heightAfter = self.heightAfterDecimal.text ?? ""
+            heightAfter = heightAfterDecimal.text ?? ""
         case 2:
-            self.weightBefore = self.weightBeforDecimal.text ?? ""
+            weightBefore = weightBeforDecimal.text ?? ""
         case 3:
-            self.weightAfter = self.weightAfterDecimal.text ?? ""
+            weightAfter = weightAfterDecimal.text ?? ""
         default:
             break
         }
         return true
+    }
+}
+
+extension BmiViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView === picker {
+            if heightBeforDecimal.isFirstResponder {
+                return heightArray[row].description
+            } else {
+                return weightArray[row].description
+            }
+        } else {
+            return decimalArray[row].description
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView === picker {
+            if heightBeforDecimal.isFirstResponder {
+                heightBeforDecimal.text = heightArray[row].description
+            }
+            if weightBeforDecimal.isFirstResponder {
+                weightBeforDecimal.text = weightArray[row].description
+            }
+        } else {
+            if heightAfterDecimal.isFirstResponder {
+                heightAfterDecimal.text = decimalArray[row].description
+            }
+            if weightAfterDecimal.isFirstResponder {
+                weightAfterDecimal.text = decimalArray[row].description
+            }
+        }
+    }
+}
+
+extension BmiViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView === picker {
+            return heightArray.count
+        } else {
+            return decimalArray.count
+        }
     }
 }
